1cbec888f1aef6ce6cbcb8326334ec3f51c0e119
saroj kumar sah <sarojahah152@gmail.com>
2023-07-02 15:06:32 +0545

    04

---
+++ 06.py
@@ -0,0 +1,10 @@
+# python program to check a number is perfect number
+a=int(input("enter a number:"))
+sum=0
+for i in range(1,a):
+    if a%i==0:
+        sum+=i
+if sum==a:
+    print(a,"is a perfect number")
+else:
+    print(a,"is not a perfect number")
